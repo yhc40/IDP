6330e71127511fb3affaac1d8b2acbc5f6d2229b
yhc40 <yhc40@cam.ac.uk>
2022-02-24 11:57:05 +0000

    rotations_make_sense_time_control

--- motor_test.cpp
+++ motor_test.cpp
@@ -20,35 +20,54 @@ void setup() {
   AFMS.begin();
 
 }
-void move_forward(double s){
+void move_forward(double s,double t){
      Motor3->setSpeed(s);
     Motor3->run(BACKWARD);
     Motor4->setSpeed(s);
     Motor4->run(FORWARD);
+    delay(t);
 }
 
-void move_backward(double s){
+void move_backward(double s,double t){
     Motor3->setSpeed(s);
     Motor3->run(FORWARD);
     Motor4->setSpeed(s);
     Motor4->run(BACKWARD);
+    delay(t);
   
 }
-void Brake(){
+void Brake(double t){
   Motor3->setSpeed(0);
   Motor4->setSpeed(0);
-  delay(3000);
+  delay(t);
    }
+// s control rotate speed, t control rotate extent theoreitcally
+void Rotate_left_inplace(double s, double t){
+    Motor3->setSpeed(s);
+    Motor3->run(FORWARD);
+    Motor4->setSpeed(s);
+    Motor4->run(FORWARD);
+    delay(t);
 
+}
+
+ void Rotate_right_inplace(double s, double t){
+    Motor3->setSpeed(s);
+    Motor3->run(BACKWARD);
+    Motor4->setSpeed(s);
+    Motor4->run(BACKWARD);
+    delay(t);
+
+ }
  
 
 void loop() {
   
-  move_forward(150);
-  delay(1000);
-  move_backward(150);
-  delay(1000);
-  Brake();
-  delay(800);
+  move_forward(150,1000);
+  
+  move_backward(150,1000);
+  
+  Brake(800);
+
 
 }
